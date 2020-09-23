e4b79ff57a158c50e56624b01d3765581d6e8cbf
Melissa <melissakheying@gmail.com>
2020-09-23 14:43:58 -0500

    :sparkles: useState hook to take form inputs to set guest
    console.log(guest)

--- client/src/components/guests/GuestForm.js
+++ client/src/components/guests/GuestForm.js
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GuestForm = () => {
+  const [guest, setGuest] = useState({
+    name: '',
+    phone: '',
+    dietary: 'Non-Veg'
+  })
+
+  const { name, phone, dietary } = guest
+
+  const handleChange = (e) => {
+    setGuest({
+      ...guest,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(guest)
+    setGuest({
+      name: '',
+      phone: '',
+      dietary: 'Non-Veg'
+    })
+  }
+
   return (
     <div className="invite-section">
       <h1>Invite Someone</h1>
-      <form >
-        <input type="text" placeholder="Name" name="name" />
-        <input type="text" placeholder="Phone" name="phone" />
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Name" name="name" value={name} onChange={handleChange} />
+        <input type="text" placeholder="Phone" name="phone" value={phone} onChange={handleChange} />
         <p className="options-label">Dietary</p>
         <div className="options">
           <label className="container">Non-veg
-        <input type="radio" name="dietary" Value='Non-Veg' />
+        <input type="radio" name="dietary" value='Non-Veg' checked={dietary === 'Non-Veg'} onChange={handleChange} />
             <span className="checkmark"></span>
           </label>
           <label className="container">Vegan
-        <input type="radio" name="dietary" Value='Vegan' />
+        <input type="radio" name="dietary" value='Vegan' onChange={handleChange} />
             <span className="checkmark"></span>
           </label>
           <label className="container">Pascatarian
-        <input type="radio" name="dietary" Value='Pesacatarian' />
+        <input type="radio" name="dietary" value='Pesacatarian' onChange={handleChange} />
             <span className="checkmark"></span>
           </label>
         </div>
