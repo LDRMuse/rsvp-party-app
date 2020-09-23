87bedc64f668bf5de73b6c375ad1958b231f7af7
Melissa <melissakheying@gmail.com>
2020-09-23 10:01:15 -0500

    :sparkles: only show confirmed or true (!filteredGuest) guests

--- client/src/components/guests/Guests.js
+++ client/src/components/guests/Guests.js
@@ -4,13 +4,13 @@ import {Guest} from './Guest'
 
 export const Guests = () => {
   //guests was passed from GuestState with useContext hook
-const {guests} = useContext(GuestContext)
+const {guests, filterGuest} = useContext(GuestContext)
 
 
-
+// take guests and filter each guest who is not a filtered guest (true) or if a guest is confirmed
   return (
     <div className="guests">
-    {guests.map(guest => <Guest key={guest.id} guest={guest}/>)}
+    {guests.filter(guest => !filterGuest || guest.isConfirmed).map(guest => <Guest key={guest.id} guest={guest}/>)}
   </div>
   )
 }
