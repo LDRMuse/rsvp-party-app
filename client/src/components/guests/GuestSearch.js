1f12f6621e0cfc33aa6d293347e61a8dd59e6e89
Melissa <melissakheying@gmail.com>
2020-09-23 10:44:06 -0500

    import useRef() hook; set searchValue to an (' '); place ref in input

--- client/src/components/guests/GuestSearch.js
+++ client/src/components/guests/GuestSearch.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useContext, useRef} from 'react'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { GuestContext } from '../../context';
 
 export const GuestSearch = () => {
+  const searchValue = useRef('')
+
+
+
   return (
     <div>
-    <input type="text" className="search" placeholder="Search Guest by name ..." />
+    <input ref={searchValue} type="text" className="search" placeholder="Search Guest by name ..." />
     <button className="button is-small"><FontAwesomeIcon icon="search" className="icon"/></button>
   </div>
   )
