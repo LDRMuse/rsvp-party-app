27f1b926070b16f48649c37063793734939e9db5
Melissa <melissakheying@gmail.com>
2020-09-23 10:58:59 -0500

    :sparkles: create conditional to show searched guests OR null
    if null, allow toggle to show guests

--- client/src/components/guests/Guests.js
+++ client/src/components/guests/Guests.js
@@ -4,13 +4,17 @@ import {Guest} from './Guest'
 
 export const Guests = () => {
   //guests was passed from GuestState with useContext hook
-const {guests, filterGuest} = useContext(GuestContext)
+const {guests, filterGuest, search} = useContext(GuestContext)
 
 
-// take guests and filter each guest who is not a filterGuest (true) or if a guest is confirmed
   return (
     <div className="guests">
-    {guests.filter(guest => !filterGuest || guest.isConfirmed).map(guest => <Guest key={guest.id} guest={guest}/>)}
+    { search !== null ? search.map(guest => <Guest key={guest.id} guest={guest}/>) :
+      guests.filter(guest => !filterGuest || guest.isConfirmed).map(guest => <Guest key={guest.id} guest={guest}/>)}
   </div>
   )
 }
+
+// if there is text in the search bar, then use search to show guests only
+// OR
+// if empty, take guests and filter each guest who is not a filterGuest (true) or if a guest is confirmed
