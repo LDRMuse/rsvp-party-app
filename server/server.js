0eedde76c63bda62c24fe92ccfc6d01207fb5ce4
Melissa <melissakheying@gmail.com>
2020-09-25 10:09:57 -0500

    create route for guests

--- server/server.js
+++ server/server.js
@@ -15,5 +15,6 @@ app.use(express.json({extended:true}))
 // this is coming from the post request in register.js route
 app.use('/register', require('./routes/register'))
 app.use('/auth', require('./routes/auth'))
+app.use('/guests', require('./routes/guests'))
 
 app.listen(process.env.PORT, () => console.info(`Server is running on port ${process.env.PORT}`))
