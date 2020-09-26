9cf408ab3105dafe32f17bc9c1fd18e56a2b2389
Melissa <melissakheying@gmail.com>
2020-09-26 11:32:16 -0500

    :building_construction: create types for authReducer

--- client/src/context/authContext/AuthState.js
+++ client/src/context/authContext/AuthState.js
@@ -1,19 +1,37 @@
 import React, { useReducer } from 'react'
+import axios from 'axios'
+
 import AuthContext from './AuthContext'
-import {authReducer} from './authReducer'
+import { authReducer } from './authReducer'
 
-
+import { SUCCESS_REGISTER, SUCCESS_LOGIN, FAIL_REGISTER, FAIL_LOGIN } from '../types'
 
 export const AuthState = (props) => {
-  const initialState ={
-    userAuth:null,
-    errors:null
+  const initialState = {
+    userAuth: null,
+    errors: null
   }
 
-const [state, dispatch] = useReducer(authReducer, initialState)
+  const [state, dispatch] = useReducer(authReducer, initialState)
 
 
+  // register user using axios
+  const registerUser = async (userData) => {
+    const config = {
+      header: {
+        'Content-Type': 'application/json'
+      }
+    }
+    //res is going to send userData and config to the /register route
+    try {
+      const res = await axios.post('/register', userData, config)
+      dispatch({
 
+      })
+    } catch (error) {
+
+    }
+  }
 
 
 
--- client/src/context/types.js
+++ client/src/context/types.js
@@ -6,3 +6,8 @@ export const DELETE_GUEST = 'DELETE_GUEST'
 export const UPDATE_GUEST = 'UPDATE_GUEST'
 export const EDIT_GUEST = 'EDIT_GUEST'
 export const CLEAR_EDIT = 'CLEAR_EDIT'
+export const SUCCESS_REGISTER = 'SUCCESS_REGISTER'
+export const SUCCESS_LOGIN = 'SUCCESS_LOGIN'
+export const FAIL_REGISTER = 'FAIL_REGISTER'
+export const FAIL_LOGIN = 'FAIL_LOGIN'
+
