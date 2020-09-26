7383f23747d5392549968dd79231f9375d768db5
Melissa <melissakheying@gmail.com>
2020-09-26 08:32:09 -0500

    :construction: import components to & use react-router-dom for routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './App.css';
-import {Home, Navbar} from './components'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {Home, Navbar, Register, Login} from './components'
 import { GuestState } from './context';
 
 
 function App() {
   return (
 <GuestState>
+<Router>
     <div>
     <Navbar />
-    <Home />
-      hello world
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/register' component={Register}/>
+      <Route exact path='/login' component={Login}/>
+    </Switch>
     </div>
+    </Router>
 </GuestState>
   );
 }
