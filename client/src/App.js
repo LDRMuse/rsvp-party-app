d744772a72e64c395cb06f269944e4648b885f2a
Melissa <melissakheying@gmail.com>
2020-09-28 10:26:59 -0500

    :sparkles: if token is in local storage, then setToken into header
    using axios

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home, Navbar, Register, Login, PrivateRoute } from './components'
 import { GuestState } from './context';
 import { AuthState } from './context/authContext'
+import { setToken } from './src/utils/setToken'
 
+// see if token is sitting in local storage
+// if there is a token, then call this and pass token to setToken to the header in axios
+if(localStorage.token) {
+  setToken(localStorage.token)
+}
 
 function App() {
   return (
